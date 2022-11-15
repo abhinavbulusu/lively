13cddad8020b6adfa1dbecdf269bc42faca602f6
abhinavbulusu <abhibulls001@gmail.com>
2022-11-15 13:12:27 -0600

    Initial Commit

---
+++ lively/livelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  livelyApp.swift
+//  lively
+//
+//  Created by Abhinav Bulusu on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct livelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
